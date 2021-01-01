134c54b708b4c82aacbec490f92496a0c57f9e83
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-02 00:31:43 +0900

    assignment Personality Quiz

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Yuki Tsukada on 2021/01/01.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [:]) {
+            (counts, answer) in counts[answer.type, default: 0] += 1
+        }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        {(pair1, pair2) in
+                return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+
+}
